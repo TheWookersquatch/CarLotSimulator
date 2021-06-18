94e0c3fda7c1616f329a86d290c123f7120dca5d
TheWookersquatch <ksprettygurl@gmail.com>
2021-06-18 12:08:42 -0600

    deleted comments

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -51,10 +51,7 @@ namespace CarLotSimulator
 
 
 
-            //Create a CarLot class
-            //It should have at least one property: a List of cars
-            //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
-            //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            
         }
     }
 }
